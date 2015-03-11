2afd74514d10376e315ccd0089880caa2d376bd2
Alexey Volkov <git@ulstu.net>
2015-03-11 21:46:29 +0300

    Popup still alive

--- home/www/js/script.js
+++ home/www/js/script.js
@@ -3,5 +3,10 @@ $( document ).ready(function() {
     $("#open-video").click(function() {
     	$(".video").fadeIn('slow');
     });
+
+    $('#myForm').submit(function () {
+    	$('#myModal').modal('hide');
+  		$("#myModal2").modal('show');
+	});
  
 });
