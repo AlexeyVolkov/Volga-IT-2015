4f12da9b068cd504ac00a794d4a8ce881fff39fe
Alexey Volkov <git@ulstu.net>
2015-03-20 13:21:34 +0300

    Blur

--- home/www/js/script.js
+++ home/www/js/script.js
@@ -9,4 +9,11 @@ $( document ).ready(function() {
   		$("#myModal2").modal('show');
 	});
  
+ 	$("#myModal").on('show.bs.modal', function() {
+ 		$(".header, .middle, .map, .about, .video, .orgs, .footer").addClass('blur');
+ 	});
+ 	$("#myModal").on('hide.bs.modal', function() {
+ 		$(".header, .middle, .map, .about, .video, .orgs, .footer").removeClass('blur');
+ 	});
+
 });
