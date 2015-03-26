74e2d32c7a4ac680cbe9d7d2251994164d52558f
Alexey Volkov <git@ulstu.net>
2015-03-26 20:02:50 +0300

    Popup

--- home/www/js/script.js
+++ home/www/js/script.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
  
-    $("#open-video").click(function() {
-    	$(".video").fadeIn('slow');
-    });
+  $("#open-video").click(function() {
+  	$(".video").fadeIn('slow');
+  });
  
  	$("#myModal").on('show.bs.modal', function() {
  		$(".header, .middle, .map, .about, .video, .orgs, .footer").addClass('blur');
@@ -10,43 +10,15 @@ $( document ).ready(function() {
  	$("#myModal").on('hide.bs.modal', function() {
  		$(".header, .middle, .map, .about, .video, .orgs, .footer").removeClass('blur');
  	});
- 	/*
-		http://www.w3schools.com/tags/att_input_pattern.asp
-		http://jsfiddle.net/Daniel_Hug/wDx62/
- 	*/
- 	/*$("#myModal form").validate({
- 		debug: true,
- 		rules: {
- 			inputName:{
- 				required: true,
- 				nowhitespace: true,
- 				letterswithbasicpunc: true,
- 				range: [1, 64]
- 			},
- 			inputSurname:{
- 				required: true,
- 				nowhitespace: true,
- 				letterswithbasicpunc: true,
- 				range: [1, 64]
- 			},
- 			inputEmail:{
- 				required: true,
- 				range: [5, 128],
- 				email: true
- 			},
- 			inputPass1:{
- 				required: true,
- 				range: [8, 32]
- 			},
- 			inputPass2:{
- 				required: true,
- 				range: [8, 32],
- 				equalTo: "#inputPass1"
- 			}
- 		},
- 		submitHandler: function(form){
- 			$('#myModal').modal('hide');
-  			$("#myModal2").modal('show');
- 		}
- 	});*/
+	$("#myForm").submit(function(event) {
+		event.preventDefault();
+		var pass1 = $("#inputPass1").val();
+		var pass2 = $("#inputPass2").val();
+		if(pass1 == pass2){
+			$("#myModal").fadeOut('slow');
+			$("#myModal2").fadeIn('slow');
+		}
+		else
+			alert("Error");
+	});
 });
